142d3c72ca5568ac33d81e272a57c401f2f7ce58
salehamunshi <117444346+salehamunshi@users.noreply.github.com>
2025-01-16 14:16:31 +0000

    created file to define the BlogPost Model using Sequelize

---
+++ models/index.js
@@ -0,0 +1,23 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('./database');
+
+const BlogPost = sequelize.define('BlogPost', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW
+    }
+});
+
+module.exports = { sequelize, BlogPost };
