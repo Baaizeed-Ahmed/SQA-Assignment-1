6d54c86f31c2d02dd36200a6c4354a2b2d9e6bc6
salehamunshi <117444346+salehamunshi@users.noreply.github.com>
2025-01-22 10:49:51 +0000

    be: adding authentication and refactoring for login and registration

--- express-pug-sqlite-blog/controllers/userController.js
+++ express-pug-sqlite-blog/controllers/userController.js
@@ -1,4 +1,3 @@
-// controllers/userController.js
 const bcrypt = require('bcrypt');
 const db = require('../models/user');
 
@@ -18,26 +17,23 @@ exports.register = (req, res) => {
   });
 };
 
-// Example login action in your userController.js
 exports.login = (req, res) => {
-    const { username, password } = req.body;
-  
-    db.get(`SELECT * FROM users WHERE username = ?`, [username], (err, user) => {
-      if (err) {
-        return res.render('login', { message: 'Something went wrong!' });
+  const { username, password } = req.body;
+  db.get(`SELECT * FROM users WHERE username = ?`, [username], (err, user) => {
+    if (err || !user) {
+      return res.render('login', { message: 'Invalid credentials' });
+    }
+    bcrypt.compare(password, user.password, (compareErr, isValid) => {
+      if (isValid) {
+        req.session.userId = user.id;
+        req.session.save(() => {
+          res.redirect('/');
+        });
+      } else {
+        res.render('login', { message: 'Incorrect password' });
       }
-      if (!user) {
-        return res.render('login', { message: 'Username does not exist' });
-      }
-  
-      bcrypt.compare(password, user.password, (err, result) => {
-        if (result) {
-          req.session.userId = user.id; // Set session userId
-          res.redirect('/home'); // Redirect to home after successful login
-        } else {
-          res.render('login', { message: 'Incorrect password' });
-        }
-      });
     });
-  };
+  });
+};
+
   
